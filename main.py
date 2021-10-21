f22049c25a5a727451ced3fdb074ff10789c63c5
Weyrich <fynn.weyrich@sap.com>
2021-10-21 13:36:15 +0200

    added comments to main

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from sensoren.AmerikanischerSensor2 import AmerikanischerTemperaturSensor2
 from SensorSockelAdapter import SensorSockelAdapter
 
 if __name__ == "__main__":
-
+    #Deutscher Sensor
     sensor_d = DeutscherTemperaturSensor()
     anschluss = TemperaturSensorAnschluss(sensor_d)
     print(anschluss.sendeDatenAnSmartHomeStation())
 
+    #Amerikanischer Sensor
     sensor_a = AmerikanischerTemperaturSensor()
     adapter1 = SensorSockelAdapter(sensor_a)
     anschluss2 = TemperaturSensorAnschluss(adapter1)
     print(anschluss2.sendeDatenAnSmartHomeStation())
 
+    #Amerikanischer Sensor 2
     sensor_a2 = AmerikanischerTemperaturSensor2()
     adapter2 = SensorSockelAdapter(sensor_a2)
     anschluss3 = TemperaturSensorAnschluss(adapter2)
